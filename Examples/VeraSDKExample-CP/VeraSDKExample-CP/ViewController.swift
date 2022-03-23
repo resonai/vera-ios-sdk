3272d26c40575a09a773b4ea876451a1131f662c
Alex Culeva <culevalex@gmail.com>
2022-03-23 18:49:28 +0200

    Add example projects

---
+++ Examples/VeraSDKExample-CP/VeraSDKExample-CP/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  VeraSDKExample-CP
+//
+//  Created by S2dent on 19.03.2022.
+//
+
+import UIKit
+import VeraSDK
+
+class ViewController: UIViewController {
+
+    private var vera: VeraViewController!
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let config = VeraConfiguration(
+            app: .init(clientID: "test"),
+            user: .init(username: nil),
+            link: .init(path: nil),
+            eventHandler: { [weak self] in self?.handleVeraEvent($0) }
+        )
+
+        let vc = VeraViewController.build(config: config)
+        self.vera = vc
+        present(vc, animated: true)
+    }
+
+    private func handleVeraEvent(_ event: VeraConfiguration.Event) {
+        switch event {
+        case .refreshToken:
+            vera.handleEvent(.updateToken(.anonymous))
+        @unknown default:
+            fatalError()
+        }
+    }
+}
+
